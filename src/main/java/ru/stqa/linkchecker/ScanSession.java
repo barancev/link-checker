c594c522eeb1467905108f414012d61c86f87826
Alexei Barantsev <barancev@gmail.com>
2017-12-31 00:41:00 +0300

    Source stab

---
+++ src/main/java/ru/stqa/linkchecker/ScanSession.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2018 Alexei Barantsev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package ru.stqa.linkchecker;
+
+public class ScanSession extends Thread {
+
+    private ScanSettings settings;
+
+    public ScanSession(ScanSettings settings) {
+        this.settings = settings;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+    }
+}
